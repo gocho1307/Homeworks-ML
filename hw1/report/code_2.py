7888d70a4cde3032f2f0caa8f1943e4aba28a216
Gocho1234 <goncalo.barias@gmail.com>
2023-09-27 15:38:14 +0100

    Add the code to the report

---
+++ hw1/report/code_2.py
@@ -0,0 +1,59 @@
+import pandas as pd, matplotlib.pyplot as plt, numpy as np
+from scipy.io.arff import loadarff
+from sklearn import metrics, tree
+from sklearn.model_selection import train_test_split
+
+# Read the ARFF file and prepare data
+data = loadarff("./data/column_diagnosis.arff")
+df = pd.DataFrame(data[0])
+df["class"] = df["class"].str.decode("utf-8")
+X, y = df.drop("class", axis=1), df["class"]
+
+DEPTH_LIMIT = [1, 2, 3, 4, 5, 6, 8, 10]
+training_accuracy, test_accuracy = [], []
+
+# Split the dataset into a testing set (30%) and a training set (70%)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, stratify=y, random_state=0
+)
+
+for depth_limit in DEPTH_LIMIT:
+    # Create and fit the decision tree classifier
+    predictor = tree.DecisionTreeClassifier(
+        max_depth=depth_limit, random_state=0
+    )
+    predictor.fit(X_train, y_train)
+
+    # Use the decision tree to predict the outcome of the given observations
+    y_train_pred = predictor.predict(X_train)
+    y_test_pred = predictor.predict(X_test)
+
+    # Get the accuracy of each test
+    train_acc = metrics.accuracy_score(y_train, y_train_pred)
+    test_acc = metrics.accuracy_score(y_test, y_test_pred)
+
+    training_accuracy.append(train_acc)
+    test_accuracy.append(test_acc)
+
+plt.plot(
+    DEPTH_LIMIT,
+    training_accuracy,
+    label="Training Accuracy",
+    marker="+",
+    color="#f8766d",
+)
+plt.plot(
+    DEPTH_LIMIT,
+    test_accuracy,
+    label="Test Accuracy",
+    marker=".",
+    color="#00bfc4",
+)
+
+plt.xlabel("Depth Limit")
+plt.ylabel("Accuracy")
+
+plt.legend()
+plt.grid(True)
+plt.savefig("./report/training_testing_accuracies.svg")
+plt.show()
