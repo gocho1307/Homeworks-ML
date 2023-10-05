f2fa190939e607f1a4d637b3b249875c7f908851
Gocho1234 <goncalo.barias@gmail.com>
2023-10-05 11:16:06 +0100

    Add partial solution for exercise 2 of Part II
    Still need to comment the plot and the code.

---
+++ hw2/report/assets/code_2.py
@@ -0,0 +1,48 @@
+import numpy as np, matplotlib.pyplot as plt, pandas as pd, seaborn as sns
+from sklearn.model_selection import StratifiedKFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from scipy.io.arff import loadarff
+
+# Read the ARFF file and prepare data
+data = loadarff("./data/column_diagnosis.arff")
+df = pd.DataFrame(data[0])
+df["class"] = df["class"].str.decode("utf-8")
+X, y = df.drop("class", axis=1), df["class"]
+
+# Initialize StratifiedKFold with 10 folds and shuffling
+folds = StratifiedKFold(n_splits=10, shuffle=True, random_state=0)
+
+# Create kNN classifiers with k=1 and k=5
+knn_1 = KNeighborsClassifier(
+    n_neighbors=1, weights="uniform", metric="euclidean"
+)
+knn_5 = KNeighborsClassifier(
+    n_neighbors=5, weights="uniform", metric="euclidean"
+)
+
+labels = ["Hernia", "Normal", "Spondylolisthesis"]
+cm_1, cm_5 = np.zeros((3, 3)), np.zeros((3, 3))
+for train_k, test_k in folds.split(X, y):
+    X_train, X_test = X.iloc[train_k], X.iloc[test_k]
+    y_train, y_test = y.iloc[train_k], y.iloc[test_k]
+
+    # Fit kNN classifiers and assess
+    knn_1.fit(X_train, y_train)
+    knn_5.fit(X_train, y_train)
+    knn_1_pred, knn_5_pred = knn_1.predict(X_test), knn_5.predict(X_test)
+    cm_1 += np.array(confusion_matrix(y_test, knn_1_pred, labels=labels))
+    cm_5 += np.array(confusion_matrix(y_test, knn_5_pred, labels=labels))
+
+# Calculate cumulative confusion matrices
+cm_diff = cm_1 - cm_5
+cm_diff_df = pd.DataFrame(cm_diff, index=labels, columns=labels)
+
+# Plot the differences
+plt.figure(figsize=(9, 7))
+sns.heatmap(
+    cm_diff_df, cmap="Purples", annot=True, annot_kws={"fontsize": 14}, fmt="g"
+)
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.show()
