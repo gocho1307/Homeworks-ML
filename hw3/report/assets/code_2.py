Fix averaging out the MLP runs

hw3/report/assets/code_2.py
@@ -9,7 +9,7 @@ X, y = data.drop("quality", axis=1), data["quality"]
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                     test_size=0.2, random_state=0)
 
-y_preds = []
+maes, maes_rb = [], []
 for rs in range(1, 11):
     # Train the MLP regressor with a specific number of iterations
     mlp = MLPRegressor(hidden_layer_sizes=(10, 10), activation="relu",
@@ -17,17 +17,14 @@ for rs in range(1, 11):
                        random_state=rs)
     mlp.fit(X_train, y_train)
 
-    # Predict the target values on the test set
-    y_preds.append(mlp.predict(X_test))
+    # Apply rounding and bounding operations
+    y_pred = mlp.predict(X_test)
+    y_pred_rb = np.clip(np.round(y_pred), 1, 10)  # Bound between 1 and 10
 
-# Apply rounding and bounding operations
-y_pred = np.mean(y_preds, axis=0)
-y_pred_rb = np.clip(np.round(y_pred), 1, 10)  # Bound between 1 and 10
-
-# Calculate MAE for both rounded and bounded predictions
-mae = mean_absolute_error(y_test, y_pred)
-mae_rb = mean_absolute_error(y_test, y_pred_rb)
+    # Calculate MAE for both rounded and bounded predictions
+    maes.append(mean_absolute_error(y_test, y_pred))
+    maes_rb.append(mean_absolute_error(y_test, y_pred_rb))
 
 # Print the MAE for both cases
-print(f"MAE without operations: {mae}")
-print(f"MAE with rounded and bounded predictions: {mae_rb}")
+print(f"MAE without operations: {np.mean(maes)}")
+print(f"MAE with rounded and bounded predictions: {np.mean(maes_rb)}")

hw3/report/assets/code_3.py
@@ -9,9 +9,9 @@ X, y = data.drop("quality", axis=1), data["quality"]
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                     test_size=0.2, random_state=0)
 
-n_iterations, rmse_scores = [20, 50, 100, 200], []
-for n_iter in n_iterations:
-    y_preds = []
+n_iters, rmse_iters = [20, 50, 100, 200], []
+for n_iter in n_iters:
+    rmse_runs = []
     for rs in range(1, 11):
         # Train the MLP regressor with a specific number of iterations
         mlp = MLPRegressor(hidden_layer_sizes=(10, 10), activation="relu",
@@ -20,12 +20,12 @@ for n_iter in n_iterations:
         mlp.fit(X_train, y_train)
 
         # Predict the target values on the test set
-        y_preds.append(mlp.predict(X_test))
+        y_pred = mlp.predict(X_test)
 
-    # Calculate RMSE
-    y_pred = np.mean(y_preds, axis=0)
-    rmse_scores.append(mean_squared_error(y_test, y_pred, squared=False))
+        # Calculate RMSE
+        rmse_runs.append(mean_squared_error(y_test, y_pred, squared=False))
+    rmse_iters.append(np.mean(rmse_runs))
 
 # Print the RMSE for the different numbers of iterations
-for i, n_iter in enumerate(n_iterations):
-    print(f"RMSE with {n_iter} iterations: {rmse_scores[i]}")
+for i, n_iter in enumerate(n_iters):
+    print(f"RMSE with {n_iter} iterations: {rmse_iters[i]}")